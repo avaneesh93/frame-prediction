5199176a6bae3f7d4023813f4da9dfe542ff9119
Avaneesh Reddy Gavva <agavva@umass.edu>
2018-12-15 11:29:13 -0500

    Changing print_losses

--- code/print_losses.py
+++ code/print_losses.py
@@ -4,7 +4,7 @@ import numpy as np
 save_path = os.path.dirname(os.getcwd()) + '/tune'
 
 for ar in os.listdir(save_path):
-    if ar[-2:] != 'lr':
+    if ar[-3:] != 'npy':
         continue
     a = np.load(os.path.join(save_path,ar))
     print('{} {}'.format(ar, a[-1]))
