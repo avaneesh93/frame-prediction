a01d6fe0190e2c11b6f0feae61b8041c3d5ae86d
Avaneesh Reddy Gavva <agavva@umass.edu>
2018-12-19 18:05:07 -0500

    Fixing baseline and removing notebook

--- code/test_model.py
+++ code/test_model.py
@@ -102,4 +102,4 @@ if __name__ == '__main__':
     data = dataset_loader(delta_t = 40.0, k = 10, offset = 10) # make sure pickled data is loaded. should use data identical/comparable to training data. 
     
     model_logger.info('Starting testing procedure now.')
-    test_model(data.X_test['walking'], data.y_test['walking'], data.delta_t, data.optical_flows_test['walking'], baseline = True)
+    test_model(data.X_test['walking'], data.y_test['walking'], data.delta_t, data.optical_flows_test['walking'], baseline = False)
